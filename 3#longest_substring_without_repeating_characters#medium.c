2034787279d9a71e2f077781855280c2007ef407
Feilx <1842068905@qq.com>
2018-09-18 23:47:14 +0800

    finish 3#

--- 3#longest_substring_without_repeating_characters#medium.c
+++ 3#longest_substring_without_repeating_characters#medium.c
@@ -1,3 +1,37 @@
 int lengthOfLongestSubstring(char* s) {
-    
+    int len = strlen(s);
+    if(len == 0)
+    	return 0;
+    int i, j, maxlen = 1, curlen = 1;
+    for(i = 0; i < len; i ++)
+    {
+    	for(j = i + 1; j < len; j ++)
+    	{
+    		int diff = 0, temp;
+    		for(temp = i; temp < j; temp++)
+    		{
+    			if(s[temp] == s[j])
+    			{
+    				diff = 1;
+    				i = temp;
+    				break;
+    			}
+    		}
+    		if(diff == 1)
+    		{
+    			diff = 0;
+    			maxlen = (curlen > maxlen) ? curlen : maxlen;
+    			curlen = 1;
+    			break;
+    		}
+    		else
+    			curlen ++;
+    	}
+    	if(j == len)
+    	{
+    		maxlen = (curlen > maxlen) ? curlen : maxlen;
+    		break;
+    	}
+    }
+    return maxlen;
 }
