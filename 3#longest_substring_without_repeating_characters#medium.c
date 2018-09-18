80c7256d0c7b130d3c02aa9319b1b6db824a2d0f
Feilx <1842068905@qq.com>
2018-09-19 01:16:03 +0800

    best for 3#

--- 3#longest_substring_without_repeating_characters#medium.c
+++ 3#longest_substring_without_repeating_characters#medium.c
@@ -1,3 +1,9 @@
+/*this solution is the best
+  just remember the postition of each character
+  and use the position to update curlen
+  you do not have to re-calculate it from zero
+  but you can use the already knew knowledge of the string
+*/
 int lengthOfLongestSubstring(char* s) {
 	int pos[128];
 	int temp;
@@ -6,31 +12,22 @@ int lengthOfLongestSubstring(char* s) {
     if(len == 0)
     	return 0;
     int i, j, maxlen = 1, curlen = 1;
-    for(i = 0; i < len; i ++)
+    for(i = 0, j = i + 1; j < len; j ++)
     {
-    	pos[s[i]] = i;
-    	for(j = i + 1; j < len; j ++)
-    	{
-    		if(pos[s[j]] != -1)
-    		{
-    			temp = i;
-    			i = pos[s[j]];
-    			for(; temp <= j; temp ++) pos[s[temp]] = -1;
-    			maxlen = (curlen > maxlen) ? curlen : maxlen;
-    			curlen = 1;
-    			break;
-    		}
-    		else
-    		{
-    			pos[s[j]] = j;
-    			curlen ++;
-    		}
-    	}
-    	if(j == len)
+   		pos[s[i]] = i;
+    	if(pos[s[j]] != -1)
     	{
     		maxlen = (curlen > maxlen) ? curlen : maxlen;
-    		break;
+    		curlen -= pos[s[j]] - i;
+    		temp = i;
+    		i = pos[s[j]] + 1;
+    		for(; temp <= i - 1; temp ++) pos[s[temp]] = -1;
     	}
+    	else
+    		curlen ++;
+    	pos[s[j]] = j;
     }
+    if(j == len)
+    	maxlen = (curlen > maxlen) ? curlen : maxlen;
     return maxlen;
 }
