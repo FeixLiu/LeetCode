9393d0324455e2b16313a502164931b8f8449b51
Feilx <1842068905@qq.com>
2018-09-19 00:41:38 +0800

    change for 3#

--- 3#longest_substring_without_repeating_characters#medium.c
+++ 3#longest_substring_without_repeating_characters#medium.c
@@ -1,31 +1,30 @@
 int lengthOfLongestSubstring(char* s) {
+	int pos[128];
+	int temp;
+	for(temp = 0; temp < 128; temp ++) pos[temp] = -1;
     int len = strlen(s);
     if(len == 0)
     	return 0;
     int i, j, maxlen = 1, curlen = 1;
     for(i = 0; i < len; i ++)
     {
+    	pos[s[i]] = i;
     	for(j = i + 1; j < len; j ++)
     	{
-    		int diff = 0, temp;
-    		for(temp = i; temp < j; temp++)
+    		if(pos[s[j]] != -1)
     		{
-    			if(s[temp] == s[j])
-    			{
-    				diff = 1;
-    				i = temp;
-    				break;
-    			}
-    		}
-    		if(diff == 1)
-    		{
-    			diff = 0;
+    			temp = i;
+    			i = pos[s[j]];
+    			for(; temp <= j; temp ++) pos[s[temp]] = -1;
     			maxlen = (curlen > maxlen) ? curlen : maxlen;
     			curlen = 1;
     			break;
     		}
     		else
+    		{
+    			pos[s[j]] = j;
     			curlen ++;
+    		}
     	}
     	if(j == len)
     	{
