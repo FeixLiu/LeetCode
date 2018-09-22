317e6b07a541cb538a6e1421582cd437d492d969
Feilx <1842068905@qq.com>
2018-09-22 08:42:03 +0800

    finish 6#

--- 6#zigzag_conversion#medium.c
+++ 6#zigzag_conversion#medium.c
@@ -1,47 +1,22 @@
 char* convert(char* s, int numRows) 
 {
-    int lens = strlen(s), groupChara = numRows * 2 - 2, groupLine = numRows - 1;
-    if(lens == 0)
-    	return "";
-    if(numRows == 1)
-    {
-    	groupChara = 1;
-    	groupLine = 1;
-    }
-    int groupNum = lens / groupChara + 1;
-    if(numRows == 1)
-        groupNum = lens;
-    int i = 0, m = 0, n = 0, down = 0;
-    char inter[numRows][groupLine * groupNum];
-    for(i = 0; i < numRows; i ++)
-    	for(m = 0; m < groupLine * groupNum; m++)
-    		inter[i][m] = '\0';
-    i = 0, m = 0;
-    while(i < lens)
-    {
-    	inter[m][n] = s[i ++];
-    	if(down == 0)
-    		if(m == numRows - 1)
-    		{
-    			down = 1, n ++, m --;
-    			if(numRows == 1)
-    				m = 0, down = 0;
-    		}
-    		else
-    			m ++;
-    	else
-    		if(m == 0)
-    			down = 0, m ++;
-    		else
-    			n ++, m --;
-    }
-    char *result = (char*)malloc((lens + 2) * sizeof(char));
-    for(i = 0, n = 0; i < numRows; i ++)
-    	for(m = 0; m < groupLine * groupNum; m++)
-    	{
-    		if(inter[i][m] != '\0')
-    			result[n ++] = inter[i][m];
-    	}
-    result[n] = '\0';
+    int lens = strlen(s) + 1;
+    int step = 2 * numRows - 2;
+    if (step < 2)
+        return s;
+    char* result = (char *)malloc(lens * sizeof(char));
+    if (!result || lens == 0 || numRows == 0)
+        return "";
+    int i, j, count = 0;
+    count = 0;
+    for (j = 0; j < numRows; j++) 
+        for (i = 0; i < (lens + numRows) ; i += step) 
+        {
+            if ( j != 0 && i > j && i - j < lens - 1 && j < numRows - 1) 
+                result[count ++] = s[i - j];
+            if ( (i + j) < lens - 1) 
+                result[count ++] = s[i + j];
+        }
+    result[count] = 0;
     return result;
 }
