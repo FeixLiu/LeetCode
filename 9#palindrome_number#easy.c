906d0739d866d063cfcd433b4df142920aa26aee
Feilx <1842068905@qq.com>
2018-09-22 09:50:32 +0800

    finish 9#

---
+++ 9#palindrome_number#easy.c
@@ -0,0 +1,40 @@
+bool isPalindrome(int x) 
+{
+	if(x < 0)
+		return false;
+	int inter = reverse(x);
+	if(inter == x)
+		return true;
+	else
+		return false;
+}
+
+int reverse(int x)
+{
+	int temp, result = 0, inter = 0;
+	if(x >= 0)
+		while(x > 0)
+		{
+			temp = x % 10;
+			x /= 10;
+			inter  = result * 10 + temp;
+			if(inter / 10 < result) //test whether the result will overflow
+				return 0;
+			result = inter;
+		}
+	else
+	{//for the negative integer
+		x = 0 - x;
+		while(x > 0)
+		{
+			temp = x % 10;
+			x /= 10;
+			inter  = result * 10 + temp;
+			if(inter / 10 < result)
+				return 0;
+			result = inter;
+		}
+		result = 0 - result;
+	}		
+	return result;
+}
