c26c6c27b81ca382c06bb260062cf0be3f6df690
Feilx <1842068905@qq.com>
2018-09-23 09:45:48 +0800

    modify 10#

--- 10#regular_expression_matching#hard.c
+++ 10#regular_expression_matching#hard.c
@@ -7,12 +7,12 @@ bool isMatch(char* s, char* p)
 	if(p[1] == '*')
 		return matchstar(s, p[0], p + 2);
 	if(*s != '\0' && (p[0] == '.' || p[0] == *s))
-		return isMatch(s + 1, p + 1);
+		return isMatch(s + 1, p + 1); //cannot use s ++ or p ++ here
 	return false;
 }
 
 bool matchstar(char* s, char c, char* p)
-{
+{ //test for the closure
 	do
 	{
 		if(isMatch(s, p))
