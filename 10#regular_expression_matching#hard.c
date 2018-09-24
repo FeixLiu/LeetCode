a13bc261fecbc116d401b826f1d868cffc46ec54
Feilx <1842068905@qq.com>
2018-09-24 10:12:14 +0800

    dynamic programming for 10#

--- 10#regular_expression_matching#hard.c
+++ 10#regular_expression_matching#hard.c
@@ -20,3 +20,46 @@ bool matchstar(char* s, char c, char* p)
 	} while(*s != '\0' && (*s ++ == c || c == '.'));
 	return false;
 }
+
+//dynamic programming method
+bool isMatch(char* s, char* p) 
+{
+	if (!s || !p)
+		return 0; 
+	int len1 = strlen(s), len2 = strlen(p), i, j;
+	char **dp = malloc(sizeof(char *) * (len1 + 1));
+	for (i = 0; i <= len1; i++)
+	{
+		dp[i] = calloc(len2 + 1, sizeof(char));
+	}
+	dp[0][0] = 1;
+	i = 0;
+	while (i < len2)
+	{
+		if (p[i] == '*' || i + 1 < len2 && p[i + 1] == '*')
+			dp[0][i + 1] = 1;
+		else
+			break;
+		i++;
+	}
+	i = 0;
+	while (i < len1)
+	{
+		j = 0;
+		while (j < len2)
+		{
+			if (s[i] == p[j] || p[j] == '.')
+			{
+				if (j + 1 < len2 && p[j + 1] == '*')
+					dp[i + 1][j + 1] = dp[i][j] | dp[i + 1][j] | dp[i][j + 1];
+				else
+					dp[i + 1][j + 1] = dp[i][j];
+			}
+			else if (j + 1 < len2 && p[j + 1] == '*' || p[j] == '*')
+				dp[i + 1][j + 1] = dp[i + 1][j];
+			j++;
+		}
+		i++;
+	}
+	return dp[len1][len2];
+}
