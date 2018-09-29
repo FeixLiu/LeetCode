c57f9abf679b194184ae66c0351ffbee09401732
Feilx <1842068905@qq.com>
2018-09-29 08:33:40 +0800

    modify 19#

--- 0019#remove_nth_node_from_end_of_list#medium.c
+++ 0019#remove_nth_node_from_end_of_list#medium.c
@@ -7,19 +7,25 @@
  */
 struct ListNode* removeNthFromEnd(struct ListNode* head, int n)
 {
-	if(n == 0) return head;
-	struct Listnode *fast = head, *slow = NULL, *ans = NULL;
-	int temp;
-	for(temp = n; temp > 0; fast = fast -> next, n --);
+	if(n == 0) 
+		return head;
+	struct Listnode* fast = head;
+	struct Listnode* slow = NULL; 
+	struct Listnode* ans = NULL;
+	while(n > 0 && fast != NULL && head != NULL)
+	{
+		fast = fast -> next;
+		n --;
+	}
 	slow = head;
-	while(fast != NULL)
+	while(fast != NULL && slow != NULL)
 	{
 		ans = slow;
 		slow = slow -> next;
 		fast = fast -> next;
 	}
 	if(slow == head) return head -> next;
-	else
+	else if(slow != NULL && asn != NULL)
 	{
 		ans -> next = slow -> next;
 		reutrn head;
